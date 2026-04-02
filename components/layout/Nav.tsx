ce53084c15cd26e012ecefc54464e090b977ac81
standwall5 <johnpatricksalen890@gmail.com>
2026-04-02 14:00:30 +0800

    add doodle to name and implement scribble effect to links

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -3,24 +3,26 @@ import { House, FolderOpenDot } from "lucide-react";
 
 const Nav = () => {
   return (
-    <nav>
-      {/* Logo */}
-      <ul>
-        {/* Home */}
-        <li>
-          <a href="">
-            <House />
-          </a>
-        </li>
+    <header>
+      <nav>
+        {/* Logo */}
+        <ul>
+          {/* Home */}
+          <li>
+            <a href="">
+              <House />
+            </a>
+          </li>
 
-        {/* Projects */}
-        <li>
-          <a href="">
-            <FolderOpenDot />
-          </a>
-        </li>
-      </ul>
-    </nav>
+          {/* Projects */}
+          <li>
+            <a href="">
+              <FolderOpenDot />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
